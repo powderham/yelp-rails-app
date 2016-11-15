0b61d11f090ca8c9469854e11368d989f7d53101
Frankie Bell <fbell.pro@gmail.com>
2016-11-15 15:44:18 +0000

    add test for sign up capability

--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -11,12 +11,22 @@ RSpec.feature 'Homepage', :type => :feature do
     visit 'yelp/index'
     expect(page).to have_content(@restaurant.name)
     expect(page).to have_content(@restaurant.description)
-    expect(page).to have_content(3)
   end
 
   scenario 'contain new restaurant button' do
     visit 'yelp/index'
-    find('input[name="new_restaurant"]').click    
+    find('button[name="new_restaurant"]').click
     expect(page).to have_current_path('/restaurant/new')
+  end
+
+  scenario 'user can sign up or log in' do
+    visit 'yelp/index'
+    find('link_to["Sign up"]').click
+    fill_in "user_email", with: 'john@email.com'
+    fill_in "user_password", with: 'password123'
+    fill_in "user_password_confirmation", with: 'password123'
+    find('input[name="commit"]').click
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_current_path('/')
   end
 end
