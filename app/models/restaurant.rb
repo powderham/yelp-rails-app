86e9ecc8bbd883213f0cba588e2072c39d7ecf65
Ed Powderham <edpowderham@outlook.com>
2016-11-15 12:09:55 +0000

    Passing homepage test with name, description and average review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,13 @@ class Restaurant < ActiveRecord::Base
   has_many :review
 
   def average_rating
-    "test"
+    @reviews = Review.all
+    count = 0
+    total = 0
+    @reviews.each do |r|
+      count += 1
+      total += r.rating
+    end
+    total/count
   end
 end
--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature 'Homepage', :type => :feature do
   before (:each) do
     @restaurant = Restaurant.create(name: 'Nanknows', description: 'Very very nice')
-    @review1 = Review.create(restaurant_id: 1, rating: 4, comment: "Was indeed very very nice")
-    @review2 = Review.create(restaurant_id: 1, rating: 2, comment: "Was less nice than advertised")
+    @review1 = Review.create(rating: 4, comment: "Was indeed very very nice")
+    @review2 = Review.create(rating: 2, comment: "Was less nice than advertised")
   end
 
   scenario 'displays restaurants on the homepage' do
@@ -12,6 +12,5 @@ RSpec.feature 'Homepage', :type => :feature do
     expect(page).to have_content(@restaurant.name)
     expect(page).to have_content(@restaurant.description)
     expect(page).to have_content(3)
-    puts page.body
   end
 end
