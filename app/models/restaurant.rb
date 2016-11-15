c7eb80b5b0b9c74ee2558a8f137f7297035bc8ff
Ed Powderham <edpowderham@outlook.com>
2016-11-15 14:50:02 +0000

    Guard against no reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,13 +2,14 @@ class Restaurant < ActiveRecord::Base
   has_many :review
 
   def average_rating
-    # @reviews = Review.all
-    # count = 0
-    # total = 0
-    # @reviews.each do |r|
-    #   count += 1
-    #   total += r.rating
-    # end
-    # total/count
+    @reviews = Review.all
+    count = 0
+    total = 0
+    @reviews.each do |r|
+      count += 1
+      total += r.rating
+    end
+    if count = 0 return "No reviews"
+    total/count
   end
 end
