ce766f7b5c971a3335a81f278e57992661eeb6bb
Julio Gonzalez <julio.gteran@gmail.com>
2016-11-18 10:07:54 +0000

    Deleting reviews and users can only create one review per restaurant

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :reviews
-  has_many :restaurants
+  has_many :reviews, dependent: :destroy
+  has_many :restaurants, dependent: :destroy
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
 
   def has_reviewed?(restaurant)
